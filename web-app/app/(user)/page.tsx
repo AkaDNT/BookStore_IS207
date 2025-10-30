8e55ca83427881e86bcb06ff399807e36d033b45
AkaDNT <nhatthong346@gmail.com>
2025-10-30 08:45:25 +0700

    refactor: add error handler for home page

--- web-app/app/(user)/page.tsx
+++ web-app/app/(user)/page.tsx
@@ -4,8 +4,12 @@ import KeyHighlights from "../components/layout/KeyHighlights";
 
 export default async function Home() {
   const res = await fetch(`${process.env.API_URL}/books`);
+  if (!res.ok) {
+    const text = await res.text().catch(() => "");
+    throw new Error(`Fetch failed ${res.status}: ${text}`);
+  }
   const books = await res.json();
-  const sortedBooks = [...books].sort((a, b) => {
+  const sortedBooks = [...books.data].sort((a, b) => {
     const dateA = a.publicationDate ? new Date(a.publicationDate).getTime() : 0;
     const dateB = b.publicationDate ? new Date(b.publicationDate).getTime() : 0;
     return dateB - dateA;
@@ -14,7 +18,7 @@ export default async function Home() {
     <div>
       <Slider></Slider>
       <KeyHighlights></KeyHighlights>
-      <ListBooks books={books} title="Selected for you"></ListBooks>
+      <ListBooks books={books.data} title="Selected for you"></ListBooks>
       <ListBooks books={sortedBooks} title="Recently update"></ListBooks>
     </div>
   );
